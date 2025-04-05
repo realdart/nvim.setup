e0725530a841b7c952fe3a365847dd77f65a717a
realdart <157936360+realdart@users.noreply.github.com>
2025-04-05 21:31:21 +0000

    changed config

--- wezterm.lua
+++ wezterm.lua
@@ -1,30 +1,53 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
-return {
-	color_scheme = "Old World",
-	font = wezterm.font("0xProto Nerd Font", { weight = "Regular" }),
-	font_size = 11.5,
-	enable_tab_bar = true,
-	use_fancy_tab_bar = false,
-	window_padding = { left = 2, right = 2, top = 1, bottom = 1 },
-	window_close_confirmation = "NeverPrompt",
-	default_cursor_style = "BlinkingBar",
-	animation_fps = 1,
+-- This table will hold the configuration.
+local config = {
+	force_reverse_video_cursor = true,
 	colors = {
-		tab_bar = {
-			active_tab = {
-				bg_color = "#1a1b26",
-				fg_color = "#a9b1d6",
-			},
-			inactive_tab = {
-				bg_color = "#16161e",
-				fg_color = "#565f89",
-			},
-		},
-	},
-	keys = {
-		{ key = "t", mods = "CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "w", mods = "CTRL", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		foreground = "#dcd7ba",
+		background = "#181616",
+
+		cursor_bg = "#c8c093",
+		cursor_fg = "#c8c093",
+		cursor_border = "#c8c093",
+
+		selection_fg = "#c8c093",
+		selection_bg = "#2d4f67",
+
+		scrollbar_thumb = "#16161d",
+		split = "#16161d",
+
+		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
 	},
 }
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+-- This is where you actually apply your config choices
+config.window_decorations = "RESIZE" -- removes window title bar
+config.force_reverse_video_cursor = true
+config.colors = {}
+config.colors.foreground = "#dcd7ba"
+config.colors.background = "#181616"
+config.colors.cursor_bg = "#c8c093"
+config.colors.cursor_fg = "#c8c093"
+config.colors.cursor_border = "#c8c093"
+config.colors.selection_fg = "#c8c093"
+config.colors.selection_bg = "#2d4f67"
+config.colors.scrollbar_thumb = "#16161d"
+config.colors.split = "#16161d"
+config.colors.ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" }
+config.colors.brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" }
+config.colors.indexed = { [16] = "#ffa066", [17] = "#ff5d62" }
+--  change
+config.window_background_opacity = 1
+config.font = wezterm.font("0xProto Nerd Font")
+config.font_size = 11.5
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
